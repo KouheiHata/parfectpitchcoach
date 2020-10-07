0b3d34ab692b9ecc030264578747fa3d8f8249c2
yamaguchi371 <telework-takefu6@outlook.jp>
2020-10-07 15:00:38 +0900

    Ｑ＆Ａ制作

---
+++ page-qa.php
@@ -0,0 +1,45 @@
+<?php /* Template Name: お母さまＱ＆Ａ */ ?>
+
+
+<?php get_header(); ?>
+
+<div class="container-fluid bg-gray">
+    <div class="container">
+        <div class="row">
+            <h2 class="heading-lg">お母さまへインタビュー</h2>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5 bb-dot">
+            <h3 class="heading-sm">鬼頭流絶対音感メソッドを始めて</h3>
+            <img src="<?php echo esc_url(get_theme_file_uri('images/q_and_a/mom1.jpg')); ?>" alt="">
+            <p class="font-weight-bold mb-3 pl-2">年中さんのお母さま</p>
+            <ul class="qa">
+                <li class="list-item list-item1">お子さんに絶対音感を身につけさせたいと思ったのは、何故ですか？</li>
+                <li class="list-item list-item2">「ピアノをより楽しく弾けたらいいと思ったからです。」</li>
+                <li class="list-item list-item3">ご両親は絶対音感をお持ちですか？</li>
+                <li class="list-item list-item4">ありません。</li>
+                <li class="list-item list-item5">お母さまはピアノ経験がありますか？</li>
+                <li class="list-item list-item6">ありません。</li>
+                <li class="list-item list-item7">お子様がコードや単音を聴き分けているのを見てどう思いますか？</li>
+                <li class="list-item list-item8">本当にビックリしています！毎日一緒に練習しているのに私はまったく分かりません</li>
+                <li class="list-item list-item9">お子さんが音を聴き分けるようになってから「すごいな！」と思うこと、他のお子さんとは違うなと思うことなど何か気づくことはありましたか？</li>
+                <li class="list-item list-item10">幼稚園で先生が弾いている曲を、家に帰って弾いているのは「すごいな！」と思います。</li>
+                <li class="list-item list-item11">もし絶対音感が身につくとしたら、お母さまも欲しい能力ですか？</li>
+                <li class="list-item list-item12">はい！欲しいです。</li>
+                <li class="list-item list-item13">これから絶対音感を使ってどのようになってほしいですか？</li>
+                <li class="list-item list-item14">ピアノをずっと楽しんで、音楽を趣味にしてほしいです。</li>
+            </ul>
+        </div>
+        
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5 bb-dot">
+            
+        </div>
+        
+    </div><!-- row end -->
+</div><!-- container end -->
+
+
+<?php get_footer(); ?>
