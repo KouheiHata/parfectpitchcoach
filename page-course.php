ab3b10253f37837184b17201b4125e7127bbc5ac
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-10-15 15:03:18 +0900

    講座ページ作成

---
+++ page-course.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+Template Name: コーチ養育講座
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<div class="container-fluid bg-gray">
+    <div class="container">
+        <div class="row">
+            <h2 class="heading-lg">ピアノ先生のための　認定コーチ資格養成講座</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container course-box pb-5">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section1">
+            <h3 class="heading-md">ピアノ教室、音楽教室で鬼頭流絶対音感メソッドを教えたい！</h3>
+            <div class="text-center">
+                <img src="<?php echo esc_url( get_theme_file_uri('images/course/img1.jpg') ); ?>" width="500px" alt="イメージ画像">
+            </div>
+            <div class="text-center">
+                <p class="d-inline-block text-left">zoomにて講座を開催行なっています<br>＊zoom が初めての方もご安心ください！</p>
+                <p class="text-center pt-3">一般社団法人絶対音感コーチ学会®&emsp;BASICコーチ</p>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section2">
+            <h3 class="heading-sm">認定コーチだけが指導できる&emsp;鬼頭流絶対音感メソッド</h3>
+            <div>
+                <img src="<?php echo esc_url( get_theme_file_uri('images/course/img2.jpg') ); ?>" alt="イメージ画像">
+            </div>
+            <div>
+                <p>認定コーチ以外が生徒に指導することはできません。<br>鬼頭流絶対音感メソッドで１５個のコード、単音を身につけさせる指導法、譜読み指導法、子育て指導法、ピアノゲーム指導法を正しく学んだ講師であるという証であり、認定校として生徒募集をし、指導することが許可されています。<br>この講座を受講され資格者として認められますと学会認定校として登録されます。<br>資格を取得してすぐ生徒募集＆レッスンが可能です。</p>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section3">
+            <h3 class="heading-sm">なぜ書籍を読んだだけで指導してはいけないの？</h3>
+            <div>
+                <img src="<?php echo esc_url( get_theme_file_uri('images/course/img3.jpg') ); ?>" alt="イメージ画像">
+            </div>
+            <div>
+                <p>書籍【子どもがどんどん賢くなる「絶対音感」の育て方】を読むと先生にとって、とても簡単に思えるレッスンかもしれません。ところがこの世は簡単に思えることほど、実は難しいものです。<br>絶対音感が身につくまでに、音に迷わなかったお子さんはほぼいません。生徒が迷ったとき、この本の内容だけで指導をしてしまうとピアノ指導経験豊富な先生でもお母様方のサポートが出来ず大変困ってしまうことになります。<br>「これなら出来そうね」と指導者が安易に考えたことでお母様やお子さんにご迷惑がかかってしまいます。<br>鬼頭流絶対音感メソッドを考案し指導していく中で、子どもたちがどのように迷い、お母様にどのようにアドバイスすれば、早く成果が出せるかという研究を長年行ってきました。<br>鬼頭流絶対音感メソッドは、お子さんに絶対音感という特別な才能を身につけるレッスンの中で親御さんにはお子さんの心を満たす親子コミュニケーション力を幼少期のうちに身に付けてほしい！という願い考案されています。<br>それは指導者としても大変学びの多い内容であり認定コーチ養成講座を学ばれた先生方からもうれしいご感想をいただいております。</p>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section4">
+            <h3 class="heading-sm">絶対音感を持たない先生も安心してご指導いただけます</h3>
+            <div>
+                <img src="<?php echo esc_url( get_theme_file_uri('images/course/img4.jpg') ); ?>" alt="イメージ画像">
+            </div>
+            <div>
+                <p>絶対音感を持たない先生方も無理なくレッスンしています。<br>ピアノをもっと楽しむために、子どもたちの耳を育てるために、絶対音感をつけてピアノを楽しめる能力を伸ばしてあげようと思われたら一緒に子どもたちの未来への才能を開いてあげてください。</p>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section5">
+            <h3 class="heading-md">絶対音感BASICコーチ養成講座の詳細</h3>
+            <div class="text-center">
+                <img src="<?php echo esc_url( get_theme_file_uri('images/course/img5.jpg') ); ?>" width="500px" alt="イメージ画像">
+            </div>
+            <h3 class="heading-sm">＊ BASICコーチで身につく指導知識 ＊</h3>
+            <div>
+                <p>①鬼頭流絶対音感メソッドを指導するBASICコーチの知識<br><br>②鬼頭流絶対音感メソッド<br>（１５個のコード、単音を聴き分ける耳の育成法）<br><br>③小さいお子さんも簡単♡<br>【早く読める！譜読み指導法】<br><br>④２歳も鍵盤シールなしでわかる！おかあさまもビックリ！<br>【鍵盤位置がわかる！ピアノ力指導法】<br><br>⑤瞬発力ピアノ①レッスン法<br><br>⑥【子どもが楽しめる指導法】<br>子どもがやりたくなる！レッスンアイデア♫<br><br>⑦子どもを知るためのBASIC知識</p>
+            </div>
+            <h3 class="heading-sm">音に迷ったときの対処指導</h3>
+            <div>
+                <p>・子どもがカードを見ない、言わないなど子どもの態度に困ったときの対処法<br><br>・音に迷うようになった、間違えるなど音が聴き取れないときの対処法<br><br>・絶対音感のための楽器知識<br><br>・お母様へのレッスンアドバイス法</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
