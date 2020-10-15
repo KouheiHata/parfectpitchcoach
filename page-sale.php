2fb1e1a491db15ef2063b970daf5fffd57b99863
KouheiHata <50469638+KouheiHata@users.noreply.github.com>
2020-10-15 14:22:16 +0900

    書籍・カード販売ページ作成

---
+++ page-sale.php
@@ -0,0 +1,236 @@
+<?php
+
+/*
+Template Name: 書籍・カード販売
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<div class="container-fluid bg-gray">
+    <div class="container">
+        <div class="row">
+            <h2 class="heading-lg">書籍・カード販売</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 kitou-top-box">
+            <h3 class="heading-md">絶対音感コーチ学会®認定　教材販売</h3>
+            <h3 class="heading-sm">書籍・絶対音感カード　　下のフォームよりご注文いただけます</h3>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <div style="float:left;width:158px;height:158px;border:1px solid #ccc;display:table-cell;text-align:center;vertical-align:middle;">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/sale/001.jpg') ); ?>" alt="イメージ画像">
+                        </div>
+                        <div style="float:left;width:158px;height:158px;border:1px solid #ccc;display:table-cell;text-align:center;vertical-align:middle;">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/sale/002.jpg') ); ?>" alt="イメージ画像">
+                        </div>
+                        <div style="float:left;width:158px;height:158px;border:1px solid #ccc;display:table-cell;text-align:center;vertical-align:middle;">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/sale/003.png') ); ?>" alt="イメージ画像">
+                        </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <h3 class="heading-sm">絶対音感は子どものIQを上げる！</h3>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/sale/004.jpg') ); ?>" alt="イメージ画像">
+                        <div class="card-body">
+                        <div class="card-text">
+                        <div class="bold pt-3">＼ おかげさまで 重版しました ／<br>
+                        <a href="https://www.amazon.co.jp/%E5%AD%90%E3%81%A9%E3%82%82%E3%81%8C%E3%81%A9%E3%82%93%E3%81%A9%E3%82%93%E8%B3%A2%E3%81%8F%E3%81%AA%E3%82%8B%E3%80%8C%E7%B5%B6%E5%AF%BE%E9%9F%B3%E6%84%9F%E3%80%8D%E3%81%AE%E8%82%B2%E3%81%A6%E6%96%B9-%E9%AC%BC%E9%A0%AD-%E6%95%AC%E5%AD%90/dp/4413112156?SubscriptionId=AKIAJLD6FH2TADXIQKDQ&tag=amebablog-a2438412-22&linkCode=xm2&camp=2025&creative=165953&creativeASIN=4413112156" class="kitou-btn2">Amazonリンクページ</a><br>
+                        　<br>
+                        絶対音感がつく訓練によって<br>
+                        　<br>
+                        「子どものIQが永久的に10ポイント以上上げられる」<br>
+                        　<br>
+                        「単語の理解力や特定の数学スキルの能力を司る<br>
+                        　脳の側頭平面を大きくする作用がある」<br></div>
+                        　<br>
+                        という研究結果が記されています。<br>
+                        <h6>カルフォルニア大学・神経生理学記憶センター研究結果</h6>
+                        　<br>
+                        （注）ごく普通の人がアインシュタインのような天才になるためのコツを紹介した「アインシュタイン・ファクター」（ウィン・ウェンガー、リチャード・ポー共著/きこ書房）より引用<br>
+                        　<br>
+                        　<br>
+                        お子さんが<span class="bold">絶対音感</span>を身につけるレッスンの中で<br>
+                        　<br>
+                        <span class="bold">「自分で考え、自分で行動できる人に育てる」鬼頭流絶対音感メソッド</span>で<br>
+                        　<br>
+                        <span class="bold">「集中力」「記憶力」「語学力」「計算力」</span>が身についたという成果が出ており<br>
+                        <span class="bold">「幼稚園でもトップの成績」</span>というお子さんが多く<br>
+                        中には<span class="bold">レッスン７ヶ月で「IQ140になった」</span>というお子さんもいます。<br>
+                        　<br>
+                        レッスン方法や上記の詳しい内容については<br>
+                        　<br>
+                        <a href="https://www.amazon.co.jp/%E5%AD%90%E3%81%A9%E3%82%82%E3%81%8C%E3%81%A9%E3%82%93%E3%81%A9%E3%82%93%E8%B3%A2%E3%81%8F%E3%81%AA%E3%82%8B%E3%80%8C%E7%B5%B6%E5%AF%BE%E9%9F%B3%E6%84%9F%E3%80%8D%E3%81%AE%E8%82%B2%E3%81%A6%E6%96%B9-%E9%AC%BC%E9%A0%AD-%E6%95%AC%E5%AD%90/dp/4413112156?SubscriptionId=AKIAJLD6FH2TADXIQKDQ&tag=amebablog-a2438412-22&linkCode=xm2&camp=2025&creative=165953&creativeASIN=4413112156" class="kitou-btn2">子どもがどんどん賢くなる<br class="d-block d-md-none">「絶対音感」の育て方（青春出版社）</a><br>
+                        　<br>
+                        をお読みください。<br>
+                        （注）鬼頭流絶対音感メソッドだけの成果であり<br>
+                        　　　このメソッドを指導できるのは<br>
+                        　　　<span class="bold">絶対音感コーチ学会認定校</span>のみです。<br>
+                        </div>  <!-- card-text -->
+                        </div>  <!-- card-body -->
+                    </div>
+                </div>
+            </div>
+            <h3 class="heading-sm">絶対音感コーチ学会®認定カード</h3>
+            <h5>＊絶対音感カードは商標登録されています。<br class="d-block d-md-none">コピー販売、類似品販売、転売等は違法です＊</h5><br>
+            <div class="card kitou-card-box pt-3">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/sale/005.jpg') ); ?>" alt="イメージ画像">
+                        <div class="card-body">
+                        <div class="card-text">
+                        絶対音感コーチメソッドのレッスンカードができました！<br>
+                        　<br>
+                        「手づくりカードはちょっと大変」というお忙しいお母様方の声がありました。<br>
+                        　<br>
+                        「手書きの文字がわからない」という子どもたちの声がありました。<br>
+                        　<br>
+                        　<br>
+                        そこで、子どもたちに文字を伝えるために<br>
+                        視覚の見え方からわかりやすく文字を伝達するデザインの<br>
+                        カードを作りました。<br>
+                        　<br>
+                        それは、子どもたちの芸術的感性を育てたいと思ったからです。<br>
+                        　<br>
+                        実は、この芸術的感性は<span class="bold">大きくなってから教えようと思っても教えられないもの</span>なのです。<br>
+                        　<br>
+                        なぜなら、<span class="bold">幼少期の頃に育つものだから</span>です。<br>
+                        　<br>
+                        幼少期はわけのわからない時期ではなく、美しいもの、センスの良いもの、いろんな色やいろんな音を見聞きして本物を知る感覚を養っていく時期なのです。<br>
+                        　<br>
+                        そのため、視覚から芸術的感性を育てたいと思い専門家にデザインをお願いしました。<br>
+                        　<br>
+                        子どもたちはカードを写真のように全体を見て記憶していますから<br>
+                        見やすい文字バランスの取れたカードで、アルファベットも覚えやすくなります。<br>
+                        　<br>
+                        裏は白紙となっておりますので、お子さんの好きなイラストや写真などを貼ってあげてください。<br>
+                        　<br>
+                        ご購入プレゼントとして<br>
+                        絶対音感カードのレッスン説明動画があります。<br>
+                        どのくらいの音で弾くのかな？どんな風にレッスンするのかな？が<br>
+                        わかる説明動画となっていますので<br>
+                        是非ご自宅でのレッスンの参考にしていただければと思います。<br>
+                        </div>  <!-- card-text -->
+                        </div>  <!-- card-body -->
+                    </div>
+                </div>
+            </div>
+            <h3 class="heading-sm">教材販売</h3>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/sale/006.jpg') ); ?>" alt="イメージ画像">
+                        <div class="card-body">
+                        <div class="card-text">
+                        <div class="bold">絶対音感カード①</div>
+                        鬼頭流絶対音感メソッドのレッスンに使う<br>
+                        コードネーム１５枚＋単音８枚がセットになっています。<br>
+                        　<br>
+                        ＊ご購入は下のフォームからお願いします。<br>
+                        </div>  <!-- card-text -->
+                        </div>  <!-- card-body -->
+
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/sale/006a.jpg') ); ?>" alt="イメージ画像">
+                        <div class="card-body">
+                        <div class="card-text">
+                        <div class="bold">絶対音感カード②</div>
+                        カード①の単音が終わったら使うカードです。<br>
+                        ヘ音記号＋ト音記号（高音）がセットになっています。<br>
+                        　<br>
+                        ＊ご購入は下のフォームからお願いします。<br>
+                        </div>  <!-- card-text -->
+                        </div>  <!-- card-body -->
+
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/sale/006b.png') ); ?>" alt="イメージ画像">
+                        <div class="card-body">
+                        <div class="card-text">
+                        書籍　子どもがどんどん賢くなる<br>
+                        　　　　「絶対音感」の育て方<br>
+                        　<br>
+                        ＊ご購入は下のフォームからお願いします。<br>
+                        </div>  <!-- card-text -->
+                        </div>  <!-- card-body -->
+                    </div>
+                </div>
+            </div>
+
+            <hr class="wp-separator">
+
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <div class="card-body">
+                        <div class="card-text">
+                        お申し込み後、3日以内にお振込先をご連絡させていただいておりますので<br>
+                        事前お振込をお願いします。確認後、発送させていただきます。<br>
+                        　<br>
+                        <span class="bold">＊認定校の生徒様</span><br>
+                        　先生へご注文してください。<br>
+                        　<br>
+                        メールが届いていない場合は、迷惑メールをご確認ください。<br>
+                        <span class="bold">info＊parfectpitchcoach.com（＊を@に変えて）のメールを受け取れるようにアドレス張に記入</span>しておくと安心です。<br>
+                        <span class="bold">特にdocomoメールは戻ってきてしまうことが多い</span>ですので受信設定又はパソコンメール等でお願いします。<br>
+                        　<br>
+                        <h5>【ご注意】＊絶対音感カードは商標登録されています。<br class="d-block d-md-none">コピー販売、類似品販売、転売等は違法です＊</h5><br>
+                        <?php echo do_shortcode('[contact-form-7 id="247" title="絶対音感_教材販売"]'); ?>
+                        </div>  <!-- card-text -->
+                        </div>  <!-- card-body -->
+                    </div>
+                </div>
+            </div>
+            <h3 class="heading-sm">おかあさまの手づくりカードをご紹介します</h3>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <div class="card-body">
+                        <div class="card-text">
+                        表面のコードや音符カードの販売を予定しておりますが<br>
+                        裏面は手づくりでお子さんの好きなキャラクターや食べ物などの<br>
+                        イラストや写真を描いたり貼ったりしてください。<br>
+                        　<br>
+                        ご参考までにお母さまの手づくりカードをご紹介させていただきます。<br>
+                        お母さまが描かれたキャラクターのイラストカードです。<br>
+                        上手に描かれていらして、ビックリしました！<br>
+                        また、お子さんがイラストを描いているカードもありました。<br>
+                        　<br>
+                        子どもたちはカードを作ってもらうととっても嬉しいようで<br>
+                        「先生見て！」と得意げな顔をして言います。<br>
+                        逆に、裏にまだ絵が貼っていないと<br>
+                        「まだ作ってないけど・・」と、とっても残念そうな顔をします。<br>
+                        　<br>
+                        お母さまが「スミマセン・・」とお子さんに仰っている姿も<br>
+                        とても微笑ましく、心が温まります。<br>
+                        　<br>
+                        お母さま、是非お子さんの好きなもので裏面を作ってみてください。<br>
+                        </div>  <!-- card-text -->
+                        </div>  <!-- card-body -->
+
+                        <div id="uramen" class="pb-5">
+                        <div><img src="<?php echo esc_url( get_theme_file_uri('images/sale/007.jpg') ); ?>" alt="裏面1" style="height:280px"></div>
+                        <div><img src="<?php echo esc_url( get_theme_file_uri('images/sale/008.jpg') ); ?>" alt="裏面2" style="height:280px"></div>
+                        <div><img src="<?php echo esc_url( get_theme_file_uri('images/sale/009.jpg') ); ?>" alt="裏面3" style="height:280px"></div>
+                        <div><img src="<?php echo esc_url( get_theme_file_uri('images/sale/010.jpg') ); ?>" alt="裏面4" style="height:280px"></div>
+                        <div><img src="<?php echo esc_url( get_theme_file_uri('images/sale/011.jpg') ); ?>" alt="裏面5" style="height:280px"></div>
+                        <div><img src="<?php echo esc_url( get_theme_file_uri('images/sale/012.jpg') ); ?>" alt="裏面6" style="height:280px"></div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
