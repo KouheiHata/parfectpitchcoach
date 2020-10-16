3b3b91dd999ee46b2cacc3f1ad2572ac0bbe3f11
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-10-16 14:50:41 +0900

    講座ページ調整

--- page-course.php
+++ page-course.php
@@ -69,6 +69,65 @@ Template Name: コーチ養育講座
             <div>
                 <p>・子どもがカードを見ない、言わないなど子どもの態度に困ったときの対処法<br><br>・音に迷うようになった、間違えるなど音が聴き取れないときの対処法<br><br>・絶対音感のための楽器知識<br><br>・お母様へのレッスンアドバイス法</p>
             </div>
+            <h3 class="heading-sm">オンライン受講６大特典</h3>
+            <div>
+                <p>１. 認定絶対音感カードプレゼント！<br>コード１５枚・単音カード（１オクターブ）　<br><br>2. 事前zoom30分付き　<br>絶対音感のご質問、お教室のご相談など<br><br>3. Facebookグループページへご招待<br>レッスンを開始して悩んでも質問ができるので安心です。<br><br>4. 絶対音感コーチ学会HPにて認定校として掲載<br><br>5. 学会ブログにてお教室をご紹介<br><br>6. 絶対音感カード（コーチ価格で購入できます）<br>①【BASICコーチ養成講座】 　50,000円（税別）　<br>②認定証・BASICサポーター認定 【認定証発行料】　3,000円(税別) <br>③【登録・初年度メソッドライセンスフィー】年間20,000円（税別）<br>＊次年度以降も同様に年間20,000円（税別）となります。<br>＊HP掲載、メソッドサポートアドバイス料含んでおります。<br>絶対音感BASICコーチ養成 オンライン講座　①②③の合計振込額　73,000円（税込80,300円）</p>
+            </div>
+            <h3 class="heading-sm">お申し込みの流れ</h3>
+            <div>
+                <p>①上記で宜しければ以下のフォームより受講希望のご連絡をお願いいたします。<br>お振込口座をお伝えさせていただきます。<br><br>②お振込確認後、事前zoom(30分）の日程をメールにてご相談となります。<br><br>③zoom後、郵便で以下を送付します。<br>・BASICコーチ養成講座テキスト<br>・絶対音感カード(特典プレゼント）<br>・メソッド使用に関する同意書（２通)<br>・返送用封筒商標登録を認定校が使用するための書類、同じ物を２通お送りしますので署名していただき１通をご返送ください。<br><br>④受講後BASICコーチ認定証を発行します。</p>
+            </div>
+            <h3 class="heading-sm">BASICコーチ養成講座後認定</h3>
+            <div>
+                <p>（社）絶対音感コーチ学会のBASICコーチ認定校ロゴが使用できるようになります<br>・絶対音感コーチ学会Facebookグループページへ招待致します。ご質問へのアドバイスされるので安心です。<br>・各種勉強会へご参加いただけます。（有料講座）</p>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section7">
+            <h3 class="heading-md">お申し込みフォーム</h3>
+            <form>
+                <div class="form-group">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputName">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section7">
+            <h3 class="heading-md">我が子に身につけさせた先生のご感想</h3>
+            <div>
+                <img src="<?php echo esc_url( get_theme_file_uri('images/course/img6.jpg') ); ?>" alt="イメージ画像">
+            </div>
+            <div>
+                <p>幼稚園児ですが楽譜を見てスラスラ出来るので曲の仕上がりが早いです。<br><br>小学生高学年でも譜読みがスラスラできない生徒がいる中で、絶対音感の訓練をしながら単音３オクターブの音域の譜読みや弾くレッスンもしていたので鍵盤位置も迷いません。<br><br>たとえ間違えたとしても音符と音の高さが聴いてわかるのですぐに自分で気づき、直せるのは本当にすごいと思います。絶対音感はおすすめです！</p>
+            </div>
+            <h3 class="heading-md">ピアノ嫌いの息子を持つ先生のご感想</h3>
+            <div>
+                <img src="<?php echo esc_url( get_theme_file_uri('images/course/img7.jpg') ); ?>" alt="イメージ画像">
+            </div>
+            <div>
+                <p>絶対音感を身につけて欲しくて我が子にレッスンしました。<br><br>ピアノ指導者あるあるですが息子はピアノ嫌いになりレッスンをずっとしていなかったので不安はありました。でも絶対音感レッスンを進めるうちに、自分でピアノの前に座るようになってきて驚きました。今ではピアノで弾きたい曲が見つかり、楽しんでいます。何より、自分に自信が持てたようでチャレンジするようになりました。生徒にも身につけて欲しいです！</p>
+            </div>
+            <div class="text-center kitou-btn-box">
+                <a class="kitou-btn" href="#">BASICコーチ養成講座受講され認定校となった先生のご感想&emsp;&emsp;こちらから</a>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section8">
+            <div class="text-center">
+                <img src="<?php echo esc_url( get_theme_file_uri('images/course/img8.png') ); ?>" alt="BASICコーチとIntermedeiateコーチ">
+            </div>
+            <div class="text-center">
+                <p class="d-inline-block text-left">BASICコーチとして鬼頭流絶対音感メソッドをご指導いただき<br>生徒さんが検定合格または受験が近い生徒へ成長させたコーチは<br>絶対音感を持った生徒に実践で使うための指導を身につける<br>Intermedeiateコーチ養成講座を受講していただけます。</p>
+            </div>
         </div>
     </div>
 </div>
