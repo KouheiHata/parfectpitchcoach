7150cd618f6ae4fc6e5b8b32229fc0bcd77850b6
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-10-19 15:03:10 +0900

    Q＆Aページ作成

--- page-qa.php
+++ page-qa.php
@@ -10,12 +10,12 @@
         </div>
     </div>
 </div>
-<div class="container">
+<div class="container pb-5">
     <div class="row">
-        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5 bb-dot">
-            <h3 class="heading-sm">鬼頭流絶対音感メソッドを始めて</h3>
-            <img src="<?php echo esc_url(get_theme_file_uri('images/q_and_a/mom1.jpg')); ?>" alt="">
-            <p class="font-weight-bold mb-3 pl-2">年中さんのお母さま</p>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5">
+            <h3 class="heading-md">鬼頭流絶対音感メソッドを始めて</h3>
+            <img src="<?php echo esc_url(get_theme_file_uri('images/q_and_a/mom1.jpg')); ?>" alt="イメージ画像">
+            <h3 class="heading-sm">年中さんのお母さま</h3>
             <ul class="qa">
                 <li class="list-item list-item1">お子さんに絶対音感を身につけさせたいと思ったのは、何故ですか？</li>
                 <li class="list-item list-item2">「ピアノをより楽しく弾けたらいいと思ったからです。」</li>
@@ -33,11 +33,168 @@
                 <li class="list-item list-item14">ピアノをずっと楽しんで、音楽を趣味にしてほしいです。</li>
             </ul>
         </div>
-        
-        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5 bb-dot">
-            
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5">
+            <img src="<?php echo esc_url(get_theme_file_uri('images/q_and_a/mom2.jpg')); ?>" alt="イメージ画像">
+            <h3 class="heading-sm">年少さんのお母さま</h3>
+            <ul class="qa">
+                <li class="list-item list-item1">お子さんに絶対音感を身につけさせたいと思ったのは、なぜですか？</li>
+                <li class="list-item list-item2">ピアノが弾けたらもっと楽しくなっただろうという親の思いがあり楽しむためにあったら良さそうに思いました。</li>
+                <li class="list-item list-item3">ご両親は絶対音感をお持ちですか？</li>
+                <li class="list-item list-item4">持っていません。</li>
+                <li class="list-item list-item5">お母さまはピアノ経験がありますか？</li>
+                <li class="list-item list-item6">経験はありません。</li>
+                <li class="list-item list-item7">お子様がコードや単音を聴き分けているのを見てどう思いますか？</li>
+                <li class="list-item list-item8">不思議な感覚です。</li>
+                <li class="list-item list-item9">お子さんが音を聴き分けるようになってから「すごいな！」と思うこと、他のお子さんとは違うなと思うことなど何か気づくことはありましたか？</li>
+                <li class="list-item list-item10">音の他にもいろいろな事に興味が広がり、調べ物をしたり、わからないことをや疑問に思ったことを考えるようになりました。</li>
+                <li class="list-item list-item11">お父さまはレッスンに関わっていますか？</li>
+                <li class="list-item list-item12">休日は私と娘のレッスンを見て、とても喜んでたくさん褒めてくれます。</li>
+                <li class="list-item list-item13">もし絶対音感が身につくとしたら、お母さまも欲しい能力ですか？</li>
+                <li class="list-item list-item14">あったらもっと楽しいのかなと思います。</li>
+                <li class="list-item list-item15">これから絶対音感を使ってどのようになってほしいですか？</li>
+                <li class="list-item list-item16">ピアノに活かして何かに悩んだり迷ったりした時のリフレッシュになればいいなぁと思っています。</li>
+            </ul>
         </div>
-        
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5">
+            <img src="<?php echo esc_url(get_theme_file_uri('images/q_and_a/mom3.jpg')); ?>" alt="イメージ画像">
+            <h3 class="heading-sm">年少さんのお母さま</h3>
+            <ul class="qa">
+                <li class="list-item list-item1">お子さんに絶対音感を身につけさせたいと思ったのは、なぜですか？</li>
+                <li class="list-item list-item2">ピアノは習わせたいと思っていたので、ピアノが弾けるようになったときに絶対音感があった方が良いかな？と思ったからです。</li>
+                <li class="list-item list-item3">ご両親は絶対音感をお持ちですか？</li>
+                <li class="list-item list-item4">持っていません。</li>
+                <li class="list-item list-item5">お母さまはピアノ経験がありますか？</li>
+                <li class="list-item list-item6">経験はありません。</li>
+                <li class="list-item list-item7">お子様がコードや単音を聴き分けているのを見てどう思いますか？</li>
+                <li class="list-item list-item8">私には全然わからないのに、どうしてわかるのだろう？と思います。</li>
+                <li class="list-item list-item9">お子さんが音を聴き分けるようになってから「すごいな！」と思うこと、他のお子さんとは違うなと思うことなど何か気づくことはありましたか？</li>
+                <li class="list-item list-item10">日常生活の違いは感じませんが、単音は幼稚園でもやっているので他のお子さんと同じくらいですがコード（和音）は全然違います！</li>
+                <li class="list-item list-item11">お父さまはレッスンに関わっていますか？</li>
+                <li class="list-item list-item12">あまり興味がないようです・・。</li>
+                <li class="list-item list-item13">もし絶対音感が身につくとしたら、お母さまも欲しい能力ですか？</li>
+                <li class="list-item list-item14">生活音を聴き分ける感覚を味わいたいので欲しいです。</li>
+                <li class="list-item list-item15">これから絶対音感を使ってどのようになってほしいですか？</li>
+                <li class="list-item list-item16">いろんな曲を弾いてほしいです。</li>
+            </ul>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5">
+            <img src="<?php echo esc_url(get_theme_file_uri('images/q_and_a/mom4.jpg')); ?>" alt="イメージ画像">
+            <h3 class="heading-sm">年少さんのお母さま</h3>
+            <ul class="qa">
+                <li class="list-item list-item1">お子さんに絶対音感を身につけさせたいと思ったのは、なぜですか？</li>
+                <li class="list-item list-item2">絶対音感を身につけることで、様々な他の能力を伸ばすことができるのでは？また、両親に全く音感がないので、身につけてあげたいと思ったからです。</li>
+                <li class="list-item list-item3">ご両親は絶対音感をお持ちですか？</li>
+                <li class="list-item list-item4">いいえ。</li>
+                <li class="list-item list-item5">お母さまはピアノ経験がありますか？</li>
+                <li class="list-item list-item6">はい。少し弾けます。</li>
+                <li class="list-item list-item7">お子様がコードや単音を聴き分けているのを見てどう思いますか？</li>
+                <li class="list-item list-item8">私が聴き分けられないので、率直にすごい！と思います。新しい音を吸収する能力が早くなっていき、羨ましいです。</li>
+                <li class="list-item list-item9">お子さんが音を聴き分けるようになってから「すごいな！」と思うこと、他のお子さんとは違うなと思うことなど何か気づくことはありましたか？</li>
+                <li class="list-item list-item10">今はまだ他のお子さまと違う面はあまりわかりませんが、ピアノや音楽に対しての興味は以前より強くなってきているような気がします！</li>
+                <li class="list-item list-item11">お父さまはレッスンに関わっていますか？</li>
+                <li class="list-item list-item12">時々一緒にレッスンをしますが、子どもの成長に驚いています。</li>
+                <li class="list-item list-item13">もし絶対音感が身につくとしたら、お母さまも欲しい能力ですか？</li>
+                <li class="list-item list-item14">思います！！！</li>
+                <li class="list-item list-item15">これから絶対音感を使ってどのようになってほしいですか？</li>
+                <li class="list-item list-item16">私自身が音楽に苦手意識を持っているので、子どもには音楽を好きになって楽しんでもらいたいです。</li>
+            </ul>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5">
+            <img src="<?php echo esc_url(get_theme_file_uri('images/q_and_a/mom2.jpg')); ?>" alt="イメージ画像">
+            <h3 class="heading-sm">年少のお母さま</h3>
+            <ul class="qa">
+                <li class="list-item list-item1">お子さんに絶対音感を身につけさせたいと思ったのは、なぜですか？</li>
+                <li class="list-item list-item2">今しか身につけることが出来ない能力なので、将来の可能性を広げておくため良い耳を持つことで、人の話をよく聞くことや、外国語習得時に役立つと考えたからです。</li>
+                <li class="list-item list-item3">ご両親は絶対音感をお持ちですか？</li>
+                <li class="list-item list-item4">いいえ。</li>
+                <li class="list-item list-item5">お母さまはピアノ経験がありますか？</li>
+                <li class="list-item list-item6">弾けません。</li>
+                <li class="list-item list-item7">お子様がコードや単音を聴き分けているのを見てどう思いますか？</li>
+                <li class="list-item list-item8">子どもの習得の速さと、いろんな働きかけでいろんな才能が開くのだと実感しています。</li>
+                <li class="list-item list-item9">お子さんが音を聴き分けるようになってから「すごいな！」と思うこと、他のお子さんとは違うなと思うことなど何か気づくことはありましたか？</li>
+                <li class="list-item list-item10">いろんな歌を早く覚えること、リズム感が良くなった気がします。ピアノの音に興味を持つようになりました。</li>
+                <li class="list-item list-item11">お父さまはレッスンに関わっていますか？</li>
+                <li class="list-item list-item12">主人はただただ感心しています。子どもと一緒にコードのカードを取る競争をしてくれます。</li>
+                <li class="list-item list-item13">もし絶対音感が身につくとしたら、お母さまも欲しい能力ですか？</li>
+                <li class="list-item list-item14">思います。子どもの好きな歌などをすぐに弾いてあげたいです。</li>
+                <li class="list-item list-item15">これから絶対音感を使ってどのようになってほしいですか？</li>
+                <li class="list-item list-item16">好きな曲を弾いたり、歌ったり、自分を表現したりコミュニケションのツールにもして欲しいなと思います。</li>
+            </ul>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5">
+            <img src="<?php echo esc_url(get_theme_file_uri('images/q_and_a/mom1.jpg')); ?>" alt="イメージ画像">
+            <h3 class="heading-sm">年少のお母さま</h3>
+            <ul class="qa">
+                <li class="list-item list-item1">お子さんに絶対音感を身につけさせたいと思ったのは、なぜですか？</li>
+                <li class="list-item list-item2">パパがピアノ経験者で、音が理解できたら楽しく音楽が身についてピアノも楽しめるようになれたらいいな♪と思いました。</li>
+                <li class="list-item list-item3">ご両親は絶対音感をお持ちですか？</li>
+                <li class="list-item list-item4">持っていません。</li>
+                <li class="list-item list-item5">お母さまはピアノ経験がありますか？</li>
+                <li class="list-item list-item6">エレクトーンの経験があります。</li>
+                <li class="list-item list-item7">お子様がコードや単音を聴き分けているのを見てどう思いますか？</li>
+                <li class="list-item list-item8">すごいな！と思います！ほんのわずかな音の差もわかっていて子どもの能力は未知だと感じました。</li>
+                <li class="list-item list-item9">お子さんが音を聴き分けるようになってから「すごいな！」と思うこと、他のお子さんとは違うなと思うことなど何か気づくことはありましたか？</li>
+                <li class="list-item list-item10">幼稚園で歌った曲やテレビから流れてきた音を聴き分けたり、口づざんだりしていて楽しそうです。世の中にはいろいろな音があることに興味があるようです。</li>
+                <li class="list-item list-item11">お父さまはレッスンに関わっていますか？</li>
+                <li class="list-item list-item12">時々レッスンをしてくれます。どちらかと言うと「褒め係」です。</li>
+                <li class="list-item list-item13">もし絶対音感が身につくとしたら、お母さまも欲しい能力ですか？</li>
+                <li class="list-item list-item14">エレクトーンを習っていた時に苦労した経験もあり、絶対音感があったら自分も変わっていたかなと思います！。</li>
+                <li class="list-item list-item15">これから絶対音感を使ってどのようになってほしいですか？</li>
+                <li class="list-item list-item16">大人になってもピアノを楽しめて、他の楽器も含め音楽が身近にあるゆとりある生活が過ごせたらいいいと思います。歌う楽しさもわかってきたので、自分を表現できたらいいと思います。</li>
+            </ul>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5">
+            <img src="<?php echo esc_url(get_theme_file_uri('images/q_and_a/mom4.jpg')); ?>" alt="イメージ画像">
+            <h3 class="heading-sm">年少のお母さま</h3>
+            <ul class="qa">
+                <li class="list-item list-item1">お子さんに絶対音感を身につけさせたいと思ったのは、なぜですか？</li>
+                <li class="list-item list-item2">色々なめんで人よりも楽しめることが多くなりそうで特に音楽が楽しめるようになって欲しい！</li>
+                <li class="list-item list-item3">ご両親は絶対音感をお持ちですか？</li>
+                <li class="list-item list-item4">持っていません。</li>
+                <li class="list-item list-item5">お母さまはピアノ経験がありますか？</li>
+                <li class="list-item list-item6">エレクトーン。</li>
+                <li class="list-item list-item7">お子様がコードや単音を聴き分けているのを見てどう思いますか？</li>
+                <li class="list-item list-item8">素晴らしい！すごいと思います！</li>
+                <li class="list-item list-item9">お子さんが音を聴き分けるようになってから「すごいな！」と思うこと、他のお子さんとは違うなと思うことなど何か気づくことはありましたか？</li>
+                <li class="list-item list-item10">少し違いますが、アルファベットを見つけるとコード名を言ったりします。たまに、CMソングなど耳に入った音を、音階で言ったりします。私には合っているのかが、わかりませんが・・。</li>
+                <li class="list-item list-item11">お父さまはレッスンに関わっていますか？</li>
+                <li class="list-item list-item12">褒めてくれます。</li>
+                <li class="list-item list-item13">もし絶対音感が身につくとしたら、お母さまも欲しい能力ですか？</li>
+                <li class="list-item list-item14">身につけられるなら、欲しいです。</li>
+                <li class="list-item list-item15">これから絶対音感を使ってどのようになってほしいですか？</li>
+                <li class="list-item list-item16">歌でも楽器でも音楽を楽しめる子になってほしいです。</li>
+            </ul>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5">
+            <img src="<?php echo esc_url(get_theme_file_uri('images/q_and_a/mom3.jpg')); ?>" alt="イメージ画像">
+            <h3 class="heading-sm">２歳のお母さま</h3>
+            <ul class="qa">
+                <li class="list-item list-item1">お子さんに絶対音感を身につけさせたいと思ったのは、なぜですか？</li>
+                <li class="list-item list-item2">この年齢ならではの能力をぜひ体験してみたいと思いました。</li>
+                <li class="list-item list-item3">ご両親は絶対音感をお持ちですか？</li>
+                <li class="list-item list-item4">いいえ。</li>
+                <li class="list-item list-item5">お母さまはピアノ経験がありますか？</li>
+                <li class="list-item list-item6">幼児期にほんの少々。</li>
+                <li class="list-item list-item7">お子様がコードや単音を聴き分けているのを見てどう思いますか？</li>
+                <li class="list-item list-item8">段々その片鱗が見え始めてきて面白いなと思いました。</li>
+                <li class="list-item list-item9">お子さんが音を聴き分けるようになってから「すごいな！」と思うこと、他のお子さんとは違うなと思うことなど何か気づくことはありましたか？</li>
+                <li class="list-item list-item10">まだそんなにはありませんが、「ピアノ頑張るー」と最近は自分から言うようなこともあるのでビックリして、凄いなぁと思います。（お友達の中にはピアノ教室の先生を見た途端に「イヤー！」という子もいるようなので）</li>
+                <li class="list-item list-item11">お父さまはレッスンに関わっていますか？</li>
+                <li class="list-item list-item12">驚いて褒めてくれます。</li>
+                <li class="list-item list-item13">もし絶対音感が身につくとしたら、お母さまも欲しい能力ですか？</li>
+                <li class="list-item list-item14">はい！欲しいです！</li>
+                <li class="list-item list-item15">これから絶対音感を使ってどのようになってほしいですか？</li>
+                <li class="list-item list-item16">いろんな場面で活用できるようになって欲しいです。</li>
+            </ul>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5">
+            <div class="text-center">
+                <img src="<?php echo esc_url(get_theme_file_uri('images/q_and_a/img1.jpg')); ?>" width="800px;" alt="イメージ画像">
+            </div>
+            <div class="text-center kitou-btn-box d-block mx-auto pt-5">
+                <a class="kitou-btn" href="#">つぎは、レッスン動画</a>
+            </div>
+        </div>
     </div><!-- row end -->
 </div><!-- container end -->
 
