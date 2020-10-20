d4da6cbef6077f27d178805bfae22291da040ade
yamaguchi371 <telework-takefu6@outlook.jp>
2020-10-20 15:00:41 +0900

    レッスン動画制作

---
+++ page-lessonvideo.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Template Name: レッスン動画
+*/
+get_header(); ?>
+
+<div class="container-fluid bg-gray">
+    <div class="container">
+        <div class="row">
+            <h2 class="heading-lg">レッスン動画</h2>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h3 class="heading-sm">絶対音感は瞬時に音を聞き分ける能力</h3>
+            <p class="">早さに驚かれますが、音が分かる子どもたちは楽しそうです♬<br>瞬時に音を聞き分けて、パーフェクトにカードを取れています。</p>
+            <h3 class="heading-sm">聴音（コードネーム）</h3>
+            <h3 class="heading-sm">聴音（単音）</h3>
+            <h3 class="heading-sm">鍵盤</h3>
+            <h3 class="heading-sm">絶対音感で１５年のピアノブランクも１時間で弾けた！</h3>
+            <p class="">ピアノを小学生で辞めてからブランク１５年、大人ピアノ。<br>楽譜の読み方、指番号、鍵盤位置からスタートしましたが<br>たった１時間でこんなに弾けるようになりました。<br>やはり記憶力の良さと指の動きを覚えるのが速いです！</p>
+            <p class="">普通の方は、こんなに短時間で弾けるようになりませんから<br>改めて絶対音感を持つ人の凄さを感じました。</p>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-12 my-5">
+           <div class="kitou-btn-box">
+               <a href="" class="kitou-btn">Youtube</a>
+           </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-12 my-5">
+            <div class="kitou-btn-box"><a href="" class="kitou-btn"></a></div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-12 my-5"></div>
+        <div class="col-lg-2 col-md-6 col-sm-12 my-5"></div>
+        <div class="col-lg-2 col-md-6 col-sm-12 my-5"></div>
+        <div class="col-lg-2 col-md-6 col-sm-12 my-5"></div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
