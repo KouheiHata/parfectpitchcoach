58f2f5c4c874bb649069abc875887796f2805b50
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-10-26 15:02:43 +0900

    TOPページ作成

--- front-page.php
+++ front-page.php
@@ -8,8 +8,121 @@ Template Name: TOPページ
 
 <?php get_header(); ?>
 
-<div class="container">
+<div class="container pb-5">
     <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-5">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="link-img">
+                        <a href="#"><img src="<?php echo esc_url( get_theme_file_uri('images/top/img1.png') ); ?>" alt="イメージ画像"></a>
+                    </div>
+                    <div>
+                        <p class="text-center pt-3">絶対音感コーチ学会について</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="link-img">
+                        <a href="#"><img src="<?php echo esc_url( get_theme_file_uri('images/top/img2.jpg') ); ?>" alt="イメージ画像"></a>
+                    </div>
+                    <div>
+                        <p class="text-center pt-3">絶対音感で才能を開く</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="link-img">
+                        <a href="#"><img src="<?php echo esc_url( get_theme_file_uri('images/top/img3.jpg') ); ?>" alt="イメージ画像"></a>
+                    </div>
+                    <div>
+                        <p class="text-center pt-3">絶対音感とは</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="link-img">
+                        <a href="#"><img src="<?php echo esc_url( get_theme_file_uri('images/top/img4.jpg') ); ?>" alt="イメージ画像"></a>
+                    </div>
+                    <div>
+                        <p class="text-center pt-3"></p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="link-img">
+                        <a href="#"><img src="<?php echo esc_url( get_theme_file_uri('images/top/img5.jpg') ); ?>" alt="イメージ画像"></a>
+                    </div>
+                    <div>
+                        <p class="text-center pt-3">絶対音感コーチ学会®認定校</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="link-img">
+                        <a href="#"><img src="<?php echo esc_url( get_theme_file_uri('images/top/img5.jpg') ); ?>" alt="イメージ画像"></a>
+                    </div>
+                    <div>
+                        <p class="text-center pt-3">絶対音感コーチ学会®認定校</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="link-img">
+                        <a href="#"><img src="<?php echo esc_url( get_theme_file_uri('images/top/img6.jpg') ); ?>" alt="イメージ画像"></a>
+                    </div>
+                    <div>
+                        <p class="text-center pt-3">書籍・絶対音感カード販売</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="link-img">
+                        <a href="#"><img src="<?php echo esc_url( get_theme_file_uri('images/top/img7.jpg') ); ?>" alt="イメージ画像"></a>
+                    </div>
+                    <div>
+                        <p class="text-center pt-3">（社）絶対音感コーチ学会®絶対音感検定について</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="link-img">
+                        <a href="#"><img src="<?php echo esc_url( get_theme_file_uri('images/top/img8.jpg') ); ?>" alt="イメージ画像"></a>
+                    </div>
+                    <div>
+                        <p class="text-center pt-3">自宅で学ぶ動画レッスン</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="link-img">
+                        <a href="#"><img src="<?php echo esc_url( get_theme_file_uri('images/top/img9.jpg') ); ?>" alt="イメージ画像"></a>
+                    </div>
+                    <div>
+                        <p class="text-center pt-3">メディア</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-5">
+            <div>
+                <h3 class="heading-md">「家族一緒の時間で  子どもの心を育む」</h3>
+                <img src="<?php echo esc_url( get_theme_file_uri('images/top/img10.jpg') ); ?>" alt="イメージ画像">
+                <div class="pt-5">
+                    <p>3,000人の子どもたちから物の見え方、音の聞こえ方、大人の言葉の感じ方などを聞き取り調査していくうちに、親子のコミュニケーションがうまくいかず、お子さんは親の愛を間違って受け取っており、お母様方はお子さんのことがわからずイライラ、モヤモヤしています。<br><br>その原因は、お互いのことを理解する知識が不足しているからです。<br><br>子どもたちがお母さんをイライラさせるのは、子どもなりの理由があります。また困った行動には、必ず原因があります。<br><br>子どもたちの心を満たすために、鬼頭流絶対音感メソッドは考案されました。<br><br>お子さんにとって普段忙しくしているお母さんが、１分間だけは自分だけを見てくれる時間だと感じることで心が満たされていきます。多くの方が持たない絶対音感だからこそ、お子さんが音を覚えていく姿に驚き目を細めて喜べることでしょう。<br>幼少期の子どもの無限の力を感じていただけることと思います。お父さんとお母さんがお子さんの成長を「一緒に見守り、褒め、喜ぶ」ために休日にはお父さんがお子さんのレッスンが出来る様に、簡単なレッスンとなっています。<br>お稽古事も育児もつい母親任せになってしまいがちですが、そんな家族の時間を一緒に過ごす習慣からお父さんもお子さんの成長を見過ごさず、育児や家事を一緒に楽しみ家族を育んでいただけたら嬉しく思います。</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-5">
+            <div>
+                <h3 class="heading-md">鬼頭式絶対音感メソッドだから　絶対音感が早く身についた！</h3>
+                <img src="<?php echo esc_url( get_theme_file_uri('images/top/img11.jpg') ); ?>" alt="イメージ画像">
+                <div class="pt-5">
+                    <p class="pb-5">＼ おかげさまで重版しました ／</p>
+                    <p>鬼頭流絶対音感メソッド<br>レッスン１回たった１分！<br><br>幼少期の脳だからこそ１日３〜４回のレッスンを推奨<br><br>お子さんの豊かな人生のために お母さまの現在の「１分」を<br>未来のお子さんに「才能」というプレゼントをしませんか？</p>
+                </div>
+                <h3 class="heading-sm">瞬時に音を聴き分けるってすごい！</h3>
+                <div>
+                    <p class="text-center">６歳までにレッスンを開始すれば<br>動画のようにに音を聴き分ける耳が育ちます</p>
+                </div>
+                <div class="mt-5">
+                    <iframe class="media-youtube" src="https://www.youtube.com/embed/T0f8mMs2XF8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div class="mt-5">
+                    <iframe class="media-youtube" src="https://www.youtube.com/embed/2bZNQP1ZpPw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
