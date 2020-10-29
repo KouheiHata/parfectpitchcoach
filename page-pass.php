fbab2b161d100addef83b59ca0643b844bd0c8ae
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-10-29 13:41:29 +0900

    合格発表ページ

---
+++ page-pass.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+Template Name: 合格発表
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<div class="container-fluid bg-gray">
+    <div class="container">
+        <div class="row">
+            <h2 class="heading-lg">合格発表</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container AandR-box pb-5">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+            <img src="<?php echo esc_url( get_theme_file_uri('images/pass/img.jpg') ); ?>" alt="合格発表">
+            <div class="pt-5">
+                <p class="text-center">おめでとうございます。よく頑張りましたね！<br><br>一般社団法人絶対音感コーチ学会®<br>代表理事 鬼頭敬子</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
